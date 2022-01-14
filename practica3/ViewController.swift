e14288cc624f5fdc2961c5af59eabe7630dcd784
noeromano <noeromano75@gmail.com>
2022-01-14 01:56:50 -0600

    se agrega lista de seleccion campo de importe total y boton para calcular propina - falta checar como del testfile sea tipo number

--- practica3/ViewController.swift
+++ practica3/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var importeTotal: UITextField!
+
+    var ListOpciones = [10,15,20]
+    var propinaSeleccionada:Int?
+    
+    @IBOutlet weak var totalPropina: UILabel!
+    @IBOutlet weak var tableViewm: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableViewm.dataSource = self
+        tableViewm.delegate = self
     }
-
-
+    @IBAction func Calcular(_ sender: Any) {
+        print("calcular")
+        /*let total:Double?
+        total = (importeTotal*propinaSeleccionada)/100
+        totalPropina.text = "total: \(total)"*/
+    }
 }
+
+
+extension ViewController: UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ListOpciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "\(ListOpciones[indexPath.row])"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(ListOpciones[indexPath.row])")
+        propinaSeleccionada = ListOpciones[indexPath.row]
+    }
+}
+    
+    
+
 
