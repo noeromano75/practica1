2ccd47bd0669ed7ce073cec52c84ae46f8e70547
noeromano <noeromano75@gmail.com>
2022-01-13 23:34:27 -0600

    se inicia Proyecto practica 3

---
+++ practica3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practica3
+//
+//  Created by Noe Romano Vazquez on 13/01/22.
+//  Copyright © 2022 noeromano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
